7a040f5539477998d605f8e560855f050ea94672
LAPTOP-CORGPLDP\Joss <joss.flandrin@gmail>
2017-11-13 13:02:53 +0100

    Voilà

---
+++ Box Project/Assets/Resources/Scripts/Box_Parameters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Parameters : MonoBehaviour {
+
+    public string faceOnGround;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OrientationDetect();
+	}
+
+    void OrientationDetect ()
+    {
+
+    }
+}
